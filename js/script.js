4642607e723d09acdbf07f985b85006fccfa324b
かいもち <86639005+kaimochipage@users.noreply.github.com>
2021-10-01 16:38:59 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 'use stript'; {
+
+  //スライダー
+  $('.slider').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+  });
+
+  //ヘッダー
+  var nav_pos = $(".header").offset().top;
+  var nav_height = $(".header").outerHeight();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > nav_pos) {
+      $("body").css("padding-top", nav_height);
+      $(".header").addClass("fixed");
+    } else {
+      $("body").css("padding-top", 0);
+      $(".header").removeClass("fixed");
+    }
+  });
 }
